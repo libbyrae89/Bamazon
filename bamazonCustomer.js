dfb62b7124609fa48cd0af3155ed6378b70621d1
Libby Hughes <libbyhughes@Libbys-MacBook-Pro.local>
2018-11-15 13:30:38 -0700

    fixed database error

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "password",
-    database: "bamazon"
+    database: "bamazondb"
 });
 
 connection.connect(function(err) {
